9ca1c761b402efecf29eda3b36200d5cf276af66
James O'Keefe <jbo88@att.net>
2021-09-02 18:10:02 -0400

    updated serverjs

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(require("./routes/api"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessTracker", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
